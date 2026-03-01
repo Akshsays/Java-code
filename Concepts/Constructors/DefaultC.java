96951977c30c779e7d3ae3015f3ca69062288d5b
elliodoming.u.e.z2.3.6.9@gmail.com <219328601+Akshsays@users.noreply.github.com>
2026-03-01 14:54:45 +0530

    Structure Changes

---
+++ Concepts/Constructors/DefaultC.java
@@ -0,0 +1,17 @@
+public class DefaultC
+{
+    int id;
+    String type;
+
+    public DefaultC()
+    {
+        this.id=1;
+        this.type="I'm a default constructor";
+    }
+    public static void main(String[] args) {
+        DefaultC myobj=new DefaultC();
+
+        System.out.println("ID:"+myobj.id);
+        System.out.println("TYPE:"+myobj.type);
+    }
+}
