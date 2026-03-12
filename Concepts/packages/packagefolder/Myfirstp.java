c350b8be8aabccea5da115481e8bd5d8ae5d0125
elliodoming.u.e.z2.3.6.9@gmail.com <219328601+Akshsays@users.noreply.github.com>
2026-03-12 13:18:31 +0530

    Package file

---
+++ Concepts/packages/packagefolder/Myfirstp.java
@@ -0,0 +1,12 @@
+package packagefolder;
+
+public class  Myfirstp
+{
+    public static void main(String[] args) {
+        System.out.println("Arguments received: " + args.length);
+        for(String arg:args)
+        {
+            System.out.println(args);
+        }
+    }
+}
