cf2e5d8378891d3969bc054e999dd93e5cd3add0
elliodoming.u.e.z2.3.6.9@gmail.com <219328601+Akshsays@users.noreply.github.com>
2026-03-17 12:52:32 +0530

    Package for Maximum Number & Power

---
+++ Problem Sets/packages/packagef/Maths.java
@@ -0,0 +1,14 @@
+package packagef;
+
+public class Maths
+{
+    public int max(int a,int b)
+    {
+        return (a >= b) ? a:b;
+    }
+
+    public double power(double base, double expo)
+    {
+        return Math.pow(base,expo);
+    }
+}
