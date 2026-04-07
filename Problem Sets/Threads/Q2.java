cdb3813cb592198f80b43fab7bd13ee1a1c6f419
elliodoming.u.e.z2.3.6.9@gmail.com <219328601+Akshsays@users.noreply.github.com>
2026-04-07 12:46:15 +0530

    Q2 completed

---
+++ Problem Sets/Threads/Q2.java
@@ -0,0 +1,25 @@
+
+class Mythread2 implements Runnable
+{
+    @Override
+    public void run()
+    {
+        for(int i=2;i<=20;i++)
+        {
+            if(i%2==0)
+            {
+                int evenn=i;
+                System.out.println("Even numbers from 2 to 20:"+evenn);
+            }
+        }
+    }
+}
+
+class Q2 
+{
+    public static void main(String[] args) {
+        Mythread2 obj=new Mythread2();
+        Thread t1=new Thread(obj);
+        t1.start();
+    }
+}
